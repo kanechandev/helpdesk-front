849c54b87cd39e99759d18ef7a302f65274a68c6
Kane Chan <kanes.chan@gmail.com>
2023-03-30 23:35:37 -0300

    Validação formulário Login.

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,3 +1,5 @@
+import { FormControl, Validators } from '@angular/forms';
+import { Credenciais } from './../../models/credenciais';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  creds: Credenciais = {
+    email: '',
+    senha: ''
+  }
+
+  email = new FormControl(null, Validators.email);
+  senha = new FormControl(null, Validators.minLength(3));
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  validaCampos(): boolean{
+    if(this.email.valid && this.senha.valid){
+      return true;
+    } else {
+      return false;
+    }
   }
 
 }
