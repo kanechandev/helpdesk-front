47517d3feff63abb24964374629d80e1a16fec54
Kane Chan <kanes.chan@gmail.com>
2023-04-05 15:48:40 -0300

    Interface para Chamado.

---
+++ src/app/models/chamado.ts
@@ -0,0 +1,12 @@
+export interface Chamado {
+    id?:            any;
+    dataAbertura?:  string,
+    prioridade?:    string,
+    status:         string;
+    titulo:         string;
+    descricao:      string;
+    tecnico:        any;
+    cliente:        any;
+    nomeCliente:    string;
+    nomeTecnico:    string;
+  }
