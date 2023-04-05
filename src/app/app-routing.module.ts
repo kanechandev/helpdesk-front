8e6bbc218db9cbb3aaf86afa5d86374e92736098
Kane Chan <kanes.chan@gmail.com>
2023-04-05 15:52:17 -0300

    Rota para Chamado-List.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,6 +12,7 @@ import { ClienteListComponent } from './components/cliente/cliente-list/cliente-
 import { ClienteCreateComponent } from './components/cliente/cliente-create/cliente-create.component';
 import { ClienteUpdateComponent } from './components/cliente/cliente-update/cliente-update.component';
 import { ClienteDeleteComponent } from './components/cliente/cliente-delete/cliente-delete.component';
+import { ChamadoListComponent } from './components/chamados/chamado-list/chamado-list.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent},
@@ -19,6 +20,7 @@ const routes: Routes = [
     path: '',
     component: NavComponent, canActivate: [AuthGuard], children: [
       {path: 'home', component: HomeComponent},
+      
       {path: 'tecnicos', component: TecnicoListComponent},
       {path: 'tecnicos/create', component: TecnicoCreateComponent},
       {path: 'tecnicos/update/:id', component: TecnicoUpdateComponent},
@@ -27,7 +29,9 @@ const routes: Routes = [
       {path: 'clientes', component:ClienteListComponent},
       {path: 'clientes/create', component:ClienteCreateComponent},
       {path: 'clientes/update/:id', component:ClienteUpdateComponent},
-      {path: 'clientes/delete/:id', component:ClienteDeleteComponent}
+      {path: 'clientes/delete/:id', component:ClienteDeleteComponent},
+
+      {path: 'chamados', component: ChamadoListComponent },
 
     ]
   }
